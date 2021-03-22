226ce77107019efab50912d9effe6f13acca45cc
Matt <mt76@mx.com>
2021-03-22 18:25:11 +0000

    BUG FIXKING

--- UTIL.SQL.SCRIPTS/sienaDealList.sql
+++ UTIL.SQL.SCRIPTS/sienaDealList.sql
@@ -2,10 +2,9 @@ SELECT        dbo.Deals.SienaReference, dbo.Deals.CustomerSienaView, dbo.Deals.S
                          dbo.Deals.MandatedUser, dbo.Deals.Portfolio, dbo.Deals.AgreementId, dbo.Deals.BackOfficeRefNo, dbo.Deals.ISIN, dbo.Deals.UTI, dbo.Book.FullName AS BookName, RIGHT(dbo.Deals.CustomerSienaView, 3) AS Centre,
                          RTRIM(LTRIM(LEFT(dbo.Deals.CustomerSienaView, 10))) AS Firm, dbo.FundamentalDealType.DealTypeShortName, dbo.Deals.FullDealType, dbo.Deals.TradeDate, dbo.Deals.DealtCcy, dbo.Deals.DealtAmount,
                          dbo.Deals.AgainstAmount, dbo.Deals.AgainstCcy, dbo.Deals.AllInRate, dbo.Deals.MktRate, dbo.Deals.SettleCcy, dbo.Deals.Direction, dbo.Deals.NpvRate, dbo.Deals.OriginUser, dbo.Deals.PayInstruction,
-                         dbo.Deals.ReceiptInstruction, dbo.Deals.NIName, { fn CONCAT(dbo.Deals.DealtCcy, dbo.Deals.AgainstCcy) } AS CCYPair, ISNULL(NULLIF (dbo.Deals.NIName, NULL), CONCAT_WS('', NULL, dbo.Deals.DealtCcy, NULL, 
-                         dbo.Deals.AgainstCcy)) AS Instrument, dbo.Portfolio.Description1 AS PortfolioName, dbo.DealRevaluation.Date AS RVDate, dbo.DealRevaluation.MarkToMarket AS RVMTM, dbo.Deals.CounterBook,
-                         Book_1.FullName AS CounterBookName, ISNULL(NULLIF (dbo.Deals.CounterBook, NULL), dbo.Deals.CustomerSienaView) AS Party, ISNULL(NULLIF (Book_1.FullName, NULL), dbo.Deals.CustomerSienaView)
-                         AS PartyName
+                         dbo.Deals.ReceiptInstruction, dbo.Deals.NIName, { fn CONCAT(dbo.Deals.DealtCcy, dbo.Deals.AgainstCcy) } AS CCYPair, ISNULL(NULLIF (dbo.Deals.NIName, NULL), { fn CONCAT(dbo.Deals.DealtCcy, dbo.Deals.AgainstCcy) }) 
+                         AS Instrument, dbo.Portfolio.Description1 AS PortfolioName, dbo.DealRevaluation.Date AS RVDate, dbo.DealRevaluation.MarkToMarket AS RVMTM, dbo.Deals.CounterBook, Book_1.FullName AS CounterBookName,
+                         ISNULL(NULLIF (dbo.Deals.CounterBook, NULL), dbo.Deals.CustomerSienaView) AS Party, ISNULL(NULLIF (Book_1.FullName, NULL), dbo.Deals.CustomerSienaView) AS PartyName
 FROM            dbo.Deals INNER JOIN
                          dbo.DealType ON dbo.Deals.FullDealType = dbo.DealType.DealTypeKey INNER JOIN
                          dbo.FundamentalDealType ON dbo.DealType.FundamentalDealTypeKey = dbo.FundamentalDealType.DealTypeKey INNER JOIN
